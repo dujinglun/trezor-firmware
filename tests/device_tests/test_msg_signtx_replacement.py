6f347dca4bebcf0ebfd30848d5e91ec8a75d4e71
Andrew Kozlik <andrew.kozlik@gmail.com>
2020-09-25 19:06:08 +0200

    test: Add device test for PayJoin.

--- tests/device_tests/test_msg_signtx_replacement.py
+++ tests/device_tests/test_msg_signtx_replacement.py
@@ -40,6 +40,15 @@ TXHASH_50f6f1 = bytes.fromhex(
 TXHASH_beafc7 = bytes.fromhex(
     "beafc7cbd873d06dbee88a7002768ad5864228639db514c81cfb29f108bb1e7a"
 )
+TXHASH_65b768 = bytes.fromhex(
+    "65b768dacccfb209eebd95a1fb80a04f1dd6a3abc6d7b41d5e9d9f91605b37d9"
+)
+TXHASH_e4b5b2 = bytes.fromhex(
+    "e4b5b24159856ea18ab5819832da3b4a6330f9c3c0a46d96674e632df504b56b"
+)
+TXHASH_70f987 = bytes.fromhex(
+    "70f9871eb03a38405cfd7a01e0e1448678132d815e2c9f552ad83ae23969509e"
+)
 
 
 @pytest.mark.skip_t1
@@ -99,4 +108,127 @@ def test_p2pkh_fee_bump(client):
         )
         _, serialized_tx = btc.sign_tx(
             client, "Bitcoin", [inp1], [out1, out2], prev_txes=TX_CACHE_MAINNET,
+        )
+
+
+@pytest.mark.skip_t1
+def test_p2wpkh_payjoin(client):
+    # Original input.
+    inp1 = messages.TxInputType(
+        address_n=parse_path("84h/1h/0h/0/0"),
+        amount=100000,
+        script_type=messages.InputScriptType.SPENDWITNESS,
+        prev_hash=TXHASH_e4b5b2,
+        prev_index=0,
+        orig_hash=TXHASH_65b768,
+        orig_index=0,
+        sequence=1516634,
+    )
+
+    # New presigned external input. (Actually 84h/1h/0h/1/1, making it easier to generate witnesses.)
+    inp2 = messages.TxInputType(
+        amount=19899859,
+        script_type=messages.InputScriptType.EXTERNAL,
+        prev_hash=TXHASH_70f987,
+        prev_index=1,
+        witness=bytes.fromhex(
+            "02483045022100eb74abb36f317d707c36d6fe1f4f73192d54417b9d5cd274e0077590833aad0a02206cf26621706aaf232c48a139910de71f7dbf17f3fb6af52a7222d19d88041e8b012102d587bc96e0ceab05f27401d66dc3e596ba02f2c0d7b018b5f80eebfaeb011012"
+        ),
+    )
+
+    # PayJoined output.
+    out1 = messages.TxOutputType(
+        address="tb1qldlynaqp0hy4zc2aag3pkenzvxy65saesxw3wd",
+        # Originally payment was 10000, now we add receiver's inp2.
+        amount=10000 + 19899859,
+        script_type=messages.OutputScriptType.PAYTOWITNESS,
+        orig_hash=TXHASH_65b768,
+        orig_index=0,
+    )
+
+    # Original change.
+    out2 = messages.TxOutputType(
+        address_n=parse_path("84h/1h/0h/1/2"),
+        amount=100000 - 10000 - 141,
+        script_type=messages.OutputScriptType.PAYTOWITNESS,
+        orig_hash=TXHASH_65b768,
+        orig_index=1,
+    )
+
+    # Expected responses when there is a sender fee bump.
+    responses = [
+        request_input(0),
+        request_meta(TXHASH_65b768),
+        request_orig_input(0, TXHASH_65b768),
+        request_input(1),
+        request_output(0),
+        request_orig_output(0, TXHASH_65b768),
+        request_output(1),
+        request_orig_output(1, TXHASH_65b768),
+        messages.ButtonRequest(code=B.SignTx),
+        messages.ButtonRequest(code=B.SignTx),
+        request_input(0),
+        request_meta(TXHASH_e4b5b2),
+        request_input(0, TXHASH_e4b5b2),
+        request_output(0, TXHASH_e4b5b2),
+        request_output(1, TXHASH_e4b5b2),
+        request_input(1),
+        request_meta(TXHASH_70f987),
+        request_input(0, TXHASH_70f987),
+        request_output(0, TXHASH_70f987),
+        request_output(1, TXHASH_70f987),
+        request_orig_input(0, TXHASH_65b768),
+        request_input(0),
+        request_input(1),
+        request_output(0),
+        request_output(1),
+        request_input(0),
+        request_input(1),
+        request_finished(),
+    ]
+
+    # Scenario 1: No fee bump by sender or receiver.
+    with client:
+        client.set_expected_responses(responses)
+        _, serialized_tx = btc.sign_tx(
+            client,
+            "Testnet",
+            [inp1, inp2],
+            [out1, out2],
+            lock_time=1516634,
+            prev_txes=TX_CACHE_TESTNET,
+        )
+
+    # Scenario 2: Sender fee bump only.
+    out1.amount = 10000 + 19899859  # Receiver does not contribute to fee.
+    out2.amount = 100000 - 10000 - 200  # Sender bumps fee from 141 to 200.
+    inp2.witness = bytes.fromhex(
+        "02483045022100af3a874c966ee595321e8699e7157f0b21f2542ddcdcafd06a9c2b4fd75e998b02206daecf235b5eb3c9dac088c904774cc0a61ac601c840efc5cbe00f99e1979a09012102d587bc96e0ceab05f27401d66dc3e596ba02f2c0d7b018b5f80eebfaeb011012"
+    )
+    with client:
+        client.set_expected_responses(responses)
+        _, serialized_tx = btc.sign_tx(
+            client,
+            "Testnet",
+            [inp1, inp2],
+            [out1, out2],
+            lock_time=1516634,
+            prev_txes=TX_CACHE_TESTNET,
+        )
+
+    # Scenario 3: Receiver fee bump.
+    out1.amount = 10000 + 19899859 - 59  # Receiver contributes 59 to fee.
+    out2.amount = 100000 - 10000 - 141  # Sender does not bump fee.
+    inp2.witness = bytes.fromhex(
+        "0248304502210097a42b35d3d16fa169667cd85a007eaf6b674495634b120d9fb62d72a0df872402203d0cdf746fd7a668276f93f660a9d052bc8a5d7cd8fea36073de38da463ece85012102d587bc96e0ceab05f27401d66dc3e596ba02f2c0d7b018b5f80eebfaeb011012"
+    )
+    with client:
+        client.set_expected_responses(responses)
+        _, serialized_tx = btc.sign_tx(
+            client,
+            "Testnet",
+            [inp1, inp2],
+            [out1, out2],
+            details=details,
+            prev_txes=TX_CACHE_TESTNET,
         )
