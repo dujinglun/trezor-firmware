f2efe96c9a0156b94cf80d345a279efc5eae1d11
Andrew Kozlik <andrew.kozlik@gmail.com>
2020-09-25 18:01:16 +0200

    test: Add device test for a replacement transaction which melds two original transactions.

--- tests/device_tests/test_msg_signtx_replacement.py
+++ tests/device_tests/test_msg_signtx_replacement.py
@@ -58,6 +58,15 @@ TXHASH_5e7667 = bytes.fromhex(
 TXHASH_efaa41 = bytes.fromhex(
     "efaa41ff3e67edf508846c1a1ed56894cfd32725c590300108f40c9edc1aac35"
 )
+TXHASH_ed89ac = bytes.fromhex(
+    "ed89acb52cfa438e3653007478e7c7feae89fdde12867943eec91293139730d1"
+)
+TXHASH_6673b7 = bytes.fromhex(
+    "6673b7248e324882b2f9d02fdd1ff1d0f9ed216a234e836b8d3ac65661cbb457"
+)
+TXHASH_927784 = bytes.fromhex(
+    "927784e07bbcefc4c738f5c31c7a739978fc86f35514edf7e7da25d53d83030b"
+)
 
 
 @pytest.mark.skip_t1
@@ -308,4 +317,141 @@ def test_p2wpkh_in_p2sh_remove_change(client):
         )
         _, serialized_tx = btc.sign_tx(
             client, "Testnet", [inp1, inp2], [out1], prev_txes=TX_CACHE_TESTNET,
+        )
+
+
+@pytest.mark.skip_t1
+def test_tx_meld(client):
+    # Meld two original transactions into one, joining the change-outputs into a different one.
+
+    inp1 = messages.TxInputType(
+        address_n=parse_path("49h/1h/0h/0/4"),
+        amount=100000,
+        script_type=messages.InputScriptType.SPENDP2SHWITNESS,
+        prev_hash=TXHASH_5e7667,
+        prev_index=1,
+        orig_hash=TXHASH_334cd7,
+        orig_index=0,
+    )
+
+    inp2 = messages.TxInputType(
+        address_n=parse_path("49h/1h/0h/0/8"),
+        amount=4973340,
+        script_type=messages.InputScriptType.SPENDP2SHWITNESS,
+        prev_hash=TXHASH_6673b7,
+        prev_index=0,
+        orig_hash=TXHASH_ed89ac,
+        orig_index=0,
+    )
+
+    inp3 = messages.TxInputType(
+        address_n=parse_path("49h/1h/0h/0/3"),
+        amount=998060,
+        script_type=messages.InputScriptType.SPENDP2SHWITNESS,
+        prev_hash=TXHASH_efaa41,
+        prev_index=0,
+        orig_hash=TXHASH_334cd7,
+        orig_index=1,
+    )
+
+    inp4 = messages.TxInputType(
+        address_n=parse_path("49h/1h/0h/0/9"),
+        amount=839318869,
+        script_type=messages.InputScriptType.SPENDP2SHWITNESS,
+        prev_hash=TXHASH_927784,
+        prev_index=0,
+        orig_hash=TXHASH_ed89ac,
+        orig_index=1,
+    )
+
+    out1 = messages.TxOutputType(
+        address="moE1dVYvebvtaMuNdXQKvu4UxUftLmS1Gt",
+        amount=100000000,
+        orig_hash=TXHASH_ed89ac,
+        orig_index=1,
+    )
+
+    out2 = messages.TxOutputType(
+        # Actually m/49'/1'/0'/0/5.
+        address="2MvUUSiQZDSqyeSdofKX9KrSCio1nANPDTe",
+        amount=1000000,
+        orig_hash=TXHASH_334cd7,
+        orig_index=0,
+    )
+
+    # Change-output. Original fees were 3780 + 90720 = 94500.
+    out3 = messages.TxOutputType(
+        address_n=parse_path("49h/1h/0h/1/0"),
+        amount=100000 + 4973340 + 998060 + 839318869 - 100000000 - 1000000 - 94500,
+        script_type=messages.OutputScriptType.PAYTOP2SHWITNESS,
+    )
+
+    with client:
+        client.set_expected_responses(
+            [
+                request_input(0),
+                request_meta(TXHASH_334cd7),
+                request_orig_input(0, TXHASH_334cd7),
+                request_input(1),
+                request_meta(TXHASH_ed89ac),
+                request_orig_input(0, TXHASH_ed89ac),
+                request_input(2),
+                request_orig_input(1, TXHASH_334cd7),
+                request_input(3),
+                request_orig_input(1, TXHASH_ed89ac),
+                request_output(0),
+                request_orig_output(0, TXHASH_ed89ac),
+                request_orig_output(1, TXHASH_ed89ac),
+                request_output(1),
+                request_orig_output(0, TXHASH_334cd7),
+                request_output(2),
+                request_orig_output(1, TXHASH_334cd7),
+                messages.ButtonRequest(code=B.SignTx),
+                messages.ButtonRequest(code=B.SignTx),
+                messages.ButtonRequest(code=B.SignTx),
+                request_input(0),
+                request_meta(TXHASH_5e7667),
+                request_input(0, TXHASH_5e7667),
+                request_output(0, TXHASH_5e7667),
+                request_output(1, TXHASH_5e7667),
+                request_input(1),
+                request_meta(TXHASH_6673b7),
+                request_input(0, TXHASH_6673b7),
+                request_input(1, TXHASH_6673b7),
+                request_input(2, TXHASH_6673b7),
+                request_input(3, TXHASH_6673b7),
+                request_input(4, TXHASH_6673b7),
+                request_output(0, TXHASH_6673b7),
+                request_input(2),
+                request_meta(TXHASH_efaa41),
+                request_input(0, TXHASH_efaa41),
+                request_output(0, TXHASH_efaa41),
+                request_input(3),
+                request_meta(TXHASH_927784),
+                request_input(0, TXHASH_927784),
+                request_input(1, TXHASH_927784),
+                request_input(2, TXHASH_927784),
+                request_output(0, TXHASH_927784),
+                request_orig_input(0, TXHASH_334cd7),
+                request_orig_input(0, TXHASH_ed89ac),
+                request_input(0),
+                request_input(1),
+                request_input(2),
+                request_input(3),
+                request_output(0),
+                request_output(1),
+                request_output(2),
+                request_input(0),
+                request_input(1),
+                request_input(2),
+                request_input(3),
+                request_finished(),
+            ]
+        )
+        _, serialized_tx = btc.sign_tx(
+            client,
+            "Testnet",
+            [inp1, inp2, inp3, inp4],
+            [out1, out2, out3],
+            prev_txes=TX_CACHE_TESTNET,
         )
